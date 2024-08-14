598f4bf581e9f5206184a4dbdaf1935f02c63348
Varsha Hindupur <hindupur.v@northeastern.edu>
2024-08-13 21:24:09 -0400

    frontend added

---
+++ frontend/src/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import os
+
+cwd = os.getcwd()
+print("cwd: ", cwd)
+
+# # Define the directory to store uploaded files relative to the current working directory
+upload_dir = os.path.join(cwd, "audio_files")
+# upload_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "audio_files")
+# print("**** upload dir: ", upload_dir)
+
+# Ensure the directory exists
+if not os.path.exists(upload_dir):
+    os.makedirs(upload_dir)
+
+# Define the directory to store uploaded files
+upload_dir = "./audio_files/"
+if not os.path.exists(upload_dir):
+    os.makedirs(upload_dir)
+
+# Streamlit file uploader
+uploaded_file = st.file_uploader("Upload your audio file", type=["mp3", "wav", "ogg"])
+
+if uploaded_file is not None:
+    # Save the uploaded file to the directory
+    file_path = os.path.join(upload_dir, uploaded_file.name)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.success(f"File saved to {file_path}")
+    
+    # You can now process the file as neededs
